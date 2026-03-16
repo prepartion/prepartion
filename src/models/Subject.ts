39a4ffdae86b3da87e7d83671d416df20c307986
bajrangipanjiyar <panjiyarbajrangi@gmail.com>
2026-03-16 12:51:15 +0530

    Fixed Subject Model

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,14 +1,12 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface ISubject extends Document {
-  classId: string; // Pehle yeh ObjectId tha, ab yeh string hoga (e.g., "class-10")
   name: string;
   price: number;
   pdfUrl?: string; // PDF link ke liye, isko abhi optional rakha hai
 }
 
 const SubjectSchema: Schema = new Schema({
-  classId: { type: String, required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   pdfUrl: { type: String, default: "" }, 
